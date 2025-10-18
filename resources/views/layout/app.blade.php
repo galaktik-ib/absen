a6bb85a86cd7483238f78153dc29ab89df737716
galaktik-ib <ggalaktik665@gmail.com>
2025-10-18 15:31:13 +0800

    erwer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>@yield('title', 'Dashboard Absen')</title>
   </head>
   <body class="bg-gray-100">
